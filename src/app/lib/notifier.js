3c5fc383cdf7aa0190ad8199c5194e0bfc225409
sammuel86 <blackpython.py@outlook.com>
2015-07-02 15:04:57 -0700

    remove logging and add more to notifyer

--- src/app/lib/notifier.js
+++ src/app/lib/notifier.js
@@ -4,20 +4,28 @@
     var Notifier = Backbone.Model.extend({
 
         initialize: function () {
-
+            this.notificationUrl = 'popcorntime.io/notifications/desktop';
         },
+        check: function () {
+            request
+                .get(this.notificationUrl)
+                .on('response', function (response) {
 
+                }).on('error', function (err) {
+                    console.log(err)
+                });
+        },
         notify: function () {
             var options = {
-                icon: "http://yourimage.jpg",
+                icon: 'file://images/icon.png',
                 body: "YTS (Movies) is currently unavailable for Popcorn Time release 0.3.7-2, but is still working for the experimental builds. We're looking into what the issue could be and why it's only affecting release 0.3.7-2"
             };
 
-            var notification = new Notification("PCT broadcast: YTS issues for Release 0.3.7-2", options);
+            var notification = new Notification('PCT broadcast: YTS issues for Release 0.3.7-2', options);
             notification.onclick = function () {
-                document.getElementById("output").innerHTML += "Notification clicked";
+                console.log('Notification clicked');
             }
-
+            /*
             notification.onshow = function () {
                 // play sound on show
                 myAud = document.getElementById("audio1");
@@ -27,7 +35,7 @@
                 setTimeout(function () {
                     notification.close();
                 }, 1000);
-            }
+            }*/
         }
 
     });

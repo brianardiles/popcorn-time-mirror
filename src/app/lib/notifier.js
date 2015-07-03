99b35caee29f3fd1b2e43bfe7be6042015f6c04f
sammuel86 <blackpython.py@outlook.com>
2015-07-03 00:55:47 -0700

    add dev mode to notifications handeler, add checks for notification target aswell

--- src/app/lib/notifier.js
+++ src/app/lib/notifier.js
@@ -3,8 +3,7 @@
 
     var Notifier = Backbone.Model.extend({
         initialize: function () {
-            App.vent.on('notification', _.bind(this.notify, this)); //we can notify now via app trigger! ||  App.vent.trigger('notification', 'title', 'content');
-
+            App.vent.on('notification', _.bind(this.notify, this)); //we can notify now via app trigger! ||  App.vent.trigger('notification', 'title', 'content', 'linkurl', 'iconurl', 'customiconurl');
             var notification = new Notification('', {
                 body: ''
             });
@@ -12,26 +11,41 @@
                 notification.close(); //we are initing the notifications so we close as soon as open
             }
         },
-        check: function () {
+        check: function (devtest) {
+            win.info('Checking for new notifications');
             var that = this;
             var nid = 0;
             this.notifications = {};
             this.fetch().then(function (n) {
                 _.each(n, function (c, i) {
-                    //if (!_.contains(Settings.seenNotifications, i)) {
-                    that.notifications[nid] = {
-                        title: c.title,
-                        content: c.content,
-                        link: c.link,
-                        id: nid
-                    };
-                    nid++;
-                    Settings.seenNotifications.push(i);
-                    //  }
+                    if (_.contains(c.targets, App.git.commit) || _.contains(c.targets, 'all')) {
+                        if (!_.contains(Settings.seenNotifications, i)) {
+                            that.notifications[nid] = {
+                                title: c.title,
+                                content: c.content,
+                                link: c.link,
+                                icon: c.icon,
+                                id: nid
+                            };
+                            nid++;
+                            Settings.seenNotifications.push(i);
+                        }
+                    } else if (devtest) {
+                        that.notifications[nid] = {
+                            title: c.title,
+                            content: c.content,
+                            link: c.link,
+                            icon: c.icon,
+                            id: nid
+                        };
+                        nid++;
+                    }
                 });
                 var notify = that.notifications[0];
-                that.notify(notify.title, notify.content, notify.link, 0);
-                AdvSettings.set('seenNotifications', Settings.seenNotifications);
+                that.notify(notify.title, notify.content, notify.link, notify.icon, 0);
+                if (!devtest) { //not setting these as seen again becouse we are doing a dev test
+                    AdvSettings.set('seenNotifications', Settings.seenNotifications);
+                }
             });
         },
         fetch: function () {
@@ -45,12 +59,13 @@
             });
             return defer.promise;
         },
-        notify: function (title, content, link, id) {
+        notify: function (title, content, link, icon, id) {
             if (!title || !content) {
                 return;
             }
             var that = this;
             var notification = new Notification(title, {
+                icon: icon,
                 body: content
             });
             notification.onclick = function () {
@@ -60,14 +75,13 @@
                     } catch (err) {
                         console.log(err)
                     }
-
                 }
             }
             notification.onerror = function (e) {
                 console.log(e);
             }
             notification.onshow = function () {
-                console.log('Showing Notification:', title);
+                win.info('Showing Notification:', title);
             }
             notification.onclose = function () {
                 if (id) {

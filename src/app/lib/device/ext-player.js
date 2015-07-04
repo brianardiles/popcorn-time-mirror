982e534d67002e197cd4f9b04f5efed36db3a047
sammuel86 <blackpython.py@outlook.com>
2015-07-04 02:25:28 -0700

    fix loading of tv subtitles for external players

--- src/app/lib/device/ext-player.js
+++ src/app/lib/device/ext-player.js
@@ -18,9 +18,8 @@
             // "" So it behaves when spaces in path
             var url = streamModel.attributes.src;
             var cmd = path.normalize('"' + this.get('path') + '" ') + getPlayerSwitches(this.get('id')) + ' ';
-            var subtitle = streamModel.attributes.subFile || '';
+            var subtitle = streamModel.attributes.subtitle || '';
             if (subtitle !== '') {
-
                 if ((this.get('id') === 'mplayer') || (this.get('id') === 'MPlayer OSX Extended')) {
                     //detect charset
                     var dataBuff = fs.readFileSync(subtitle);

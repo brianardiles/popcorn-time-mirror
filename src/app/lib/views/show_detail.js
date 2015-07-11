b420aedc5cf91c8eca438f7aa06cdb62cd86aec1
sammuel86 <blackpython.py@outlook.com>
2015-07-10 20:41:36 -0700

    add correct font to styl, add seasons tab js

--- src/app/lib/views/show_detail.js
+++ src/app/lib/views/show_detail.js
@@ -44,6 +44,93 @@
                 $('.quality-toggle p').removeClass('active');
                 $(this).addClass('active');
             });
+
+
+            //change season
+            $('.seasons-container li').on('click', function (e) {
+                e.preventDefault();
+                $('.seasons-container li').removeClass('active');
+                $(this).addClass('active');
+                var seasonId = $(this).data('id');
+                var posterURL = $(this).data('poster');
+                $('.poster').attr("src", posterURL);
+                $('.episode-list-show').removeClass('episode-list-show');
+                $('#season-' + seasonId).addClass('episode-list-show').find("li.active").click();
+
+            });
+
+            //change active episode on list
+            $('.episode-container ul li').on('click', function (e) {
+                $('.episode-container ul li').removeClass('active');
+                $(this).addClass('active');
+
+            });
+
+
+            //owl.owlCarousel
+            var owl = $(".seasons-container");
+            owl.owlCarousel({
+                pagination: false, //boolean   Show pagination.
+                responsive: false,
+                autoWidth: true
+            });
+
+            function recalcCarouselWidth(carousel) {
+                var stage = carousel.find('.owl-stage');
+                stage.width(Math.ceil(stage.width()) + 1);
+            }
+
+            $(window).on('resize', function (e) {
+                recalcCarouselWidth($('.owl-carousel'));
+            }).resize();
+
+            $('.seasons-container').on('refreshed.owl.carousel', function (event) {
+                recalcCarouselWidth($('.owl-carousel'));
+            });
+
+
+            // Custom Navigation Events
+            $(".season-next").click(function (e) {
+                e.preventDefault();
+                owl.trigger('next.owl.carousel');
+            });
+            $(".season-prev").click(function (e) {
+                e.preventDefault();
+                owl.trigger('prev.owl.carousel');
+            });
+            // possibility of scroll with the scroll wheel
+            owl.on('mousewheel', '.owl-stage', function (e) {
+                e.preventDefault();
+                if (e.deltaY > 0) {
+                    owl.trigger('next.owl');
+                } else {
+                    owl.trigger('prev.owl');
+                }
+            });
+
+            var seasons_container_width = $('.seasons-wrapper').width();
+            var seasons_items_width = $('.seasons-wrapper ul li').width() * $('.seasons-wrapper ul li').length;
+            console.log(seasons_container_width, seasons_items_width);
+            if (seasons_container_width > seasons_items_width) {
+                $(".season-prev").hide();
+                $(".season-next").hide();
+            } else {
+                $(".season-prev").show();
+                $(".season-next").show();
+            }
+            $(window).resize(function () {
+                var seasons_container_width = $('.seasons-wrapper').width();
+                var seasons_items_width = $('.seasons-wrapper ul li').width() * $('.seasons-wrapper ul li').length;
+                if (seasons_container_width > seasons_items_width) {
+                    $(".season-prev").hide();
+                    $(".season-next").hide();
+                } else {
+                    $(".season-prev").show();
+                    $(".season-next").show();
+                }
+            });
+
+
         },
 
 

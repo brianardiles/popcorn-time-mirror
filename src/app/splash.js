269a2a7bf1ae1bcfb08103badac323833b2f419c
watdafox <watdafox@popcorntime.io>
2015-07-23 19:14:32 +0200

    splash again

--- src/app/splash.js
+++ src/app/splash.js
@@ -5,7 +5,7 @@ var splashwin = require('nw.gui').Window.open('splash.html', {
     'position': 'center', // centered
     'width': Math.round(window.screen.width * 0.24) || 460,
     'resizable': false,
-    'height': Math.round(window.screen.width * 0.09) || 180,
+    'height': Math.round(window.screen.width * 0.13) || 250,
     'transparent': true,
     'always-on-top': true // always on top
 });
